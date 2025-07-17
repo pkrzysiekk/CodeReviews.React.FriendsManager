463f426cba45e62c3be80e978cb0448323dafd81
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-17 14:33:03 +0200

    Implement Category controller

---
+++ FriendsManager.pkrzysiekk/FriendsAPI/FriendsAPI/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+using FriendsAPI.Models;
+using FriendsAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsAPI.Controllers;
+
+public class CategoryController : ControllerBase
+{
+    private readonly IService<Category> _service;
+
+    public CategoryController(IService<Category> service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Category>>> GetPagedEntities(int pageSize, int pageNumber)
+    {
+        var pagedResult = await _service.GetPagedEntities(pageSize, pageNumber);
+        return Ok(pagedResult);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Category>> GetCategory(int id)
+    {
+        Category cat = new();
+        try
+        {
+            cat = await _service.GetEntityById(id);
+        }
+        catch
+        {
+            return BadRequest();
+        }
+        if(cat == null)
+            return NotFound();
+        return Ok(cat);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> PostEntity([FromBody] Category cat)
+    {
+        try
+        {
+            await _service.AddEntity(cat);
+        }
+        catch
+        {
+            return BadRequest();
+        }
+        return Ok();
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult> UpdateEntity([FromBody] Category cat)
+    {
+        try
+        {
+            await _service.UpdateEntity(cat);
+        }
+        catch
+        {
+            return BadRequest();
+        }
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteEntity(int id)
+    {
+        try
+        {
+            await _service.DeleteEntity(id);
+        }
+        catch
+        {
+            return BadRequest();
+        }
+        return Ok();
+    }
+}
--- FriendsManager.pkrzysiekk/FriendsAPI/FriendsAPI/Controllers/FriendsController.cs
+++ FriendsManager.pkrzysiekk/FriendsAPI/FriendsAPI/Controllers/FriendsController.cs
@@ -70,6 +70,7 @@ public class FriendsController : ControllerBase
         {
             return BadRequest();
         }
+
         return Ok();
     }
 }
