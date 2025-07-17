8c427ea3a21ded496445a14076e6125cc28782c6
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-17 14:05:30 +0200

    Add DI to Program.cs

--- FriendsManager.pkrzysiekk/FriendsAPI/FriendsAPI/Program.cs
+++ FriendsManager.pkrzysiekk/FriendsAPI/FriendsAPI/Program.cs
@@ -1,4 +1,7 @@
 using FriendsAPI.Data;
+using FriendsAPI.Models;
+using FriendsAPI.Repository;
+using FriendsAPI.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<FriendsContext>();
+
+//DI
+builder.Services.AddScoped<IRepository<Friend>, FriendsRepository>();
+builder.Services.AddScoped<IService<Friend>, FriendService>();
+
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+builder.Services.AddScoped<IService<Category>, CategoryService>();
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
