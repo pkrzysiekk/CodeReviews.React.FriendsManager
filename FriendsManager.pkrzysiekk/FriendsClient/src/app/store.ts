b6807f2e9da61661e149167cc6e141d948176849
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-22 16:14:31 +0200

    Add store

--- FriendsManager.pkrzysiekk/FriendsClient/src/app/store.ts
+++ FriendsManager.pkrzysiekk/FriendsClient/src/app/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { categoriesSlice } from "../components/categories/categoriesSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
-    friends: friendsReducer,
-    categories: categoriesReducer,
+    categories: categoriesSlice.reducer,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<AppStore["getState"]>;
 export type AppStore = typeof store;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
