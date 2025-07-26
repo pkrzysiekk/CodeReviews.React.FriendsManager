a23bcc27ad08193c5f8b50f7fd29941f5682af8d
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-26 13:07:44 +0200

    Add Bootstrap 5 to project

--- FriendsManager.pkrzysiekk/FriendsClient/vite.config.ts
+++ FriendsManager.pkrzysiekk/FriendsClient/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // ← to jest wymagane!
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+});
