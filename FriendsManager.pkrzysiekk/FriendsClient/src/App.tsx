f1c44613a8575ade3d7d0ea843dc879e65705215
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-28 16:19:34 +0200

    Add React router

--- FriendsManager.pkrzysiekk/FriendsClient/src/App.tsx
+++ FriendsManager.pkrzysiekk/FriendsClient/src/App.tsx
@@ -2,12 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import CategoriesComponent from "./components/categories/CategoriesComponent";
 import FriendsComponent from "./components/friends/FriendsComponent";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Router,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <FriendsComponent />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/friends" />} />
+        <Route path="/friends" element={<FriendsComponent />} />
+        <Route path="/categories" element={<CategoriesComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
