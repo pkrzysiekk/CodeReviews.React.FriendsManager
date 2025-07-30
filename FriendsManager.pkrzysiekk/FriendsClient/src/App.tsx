d7d1d9c4836354de8dc42514f8431b64c1f078a6
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-30 12:28:20 +0200

    Add new DOM elements and style for new layout

--- FriendsManager.pkrzysiekk/FriendsClient/src/App.tsx
+++ FriendsManager.pkrzysiekk/FriendsClient/src/App.tsx
@@ -14,15 +14,19 @@ import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/friends" />} />
-        <Route path="/friends" element={<FriendsComponent />} />
-        <Route path="/categories" element={<CategoriesComponent />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div className="app-wrapper">
+      <BrowserRouter>
+        <Navbar />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/friends" />} />
+            <Route path="/friends" element={<FriendsComponent />} />
+            <Route path="/categories" element={<CategoriesComponent />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
